78496cd6a976ec211739b6c3478bd81207bc8365
Vadim Sohin , DevOps  Engineer <vadim.sohin@gmail.com>
2019-05-05 13:50:27 +0300

    Create main.tf
    example file to create ec2 instance

---
+++ main.tf
@@ -0,0 +1,32 @@
+# Provider
+ provider "aws" {
+   access_key = ""
+   secret_key = ""
+   region = ""
+ }
+
+ data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+  key_name = ""
+
+  tags = {
+    Name = ""
+  }
+}
