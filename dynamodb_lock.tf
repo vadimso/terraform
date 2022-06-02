793798419329284cbf827aaa4ac4bb86f6ef057a
Vadim Sohin , DevOps  Engineer <vadim.sohin@gmail.com>
2022-06-02 12:25:23 +0300

    Create dynamodb_lock.tf

---
+++ dynamodb_lock.tf
@@ -0,0 +1,33 @@
+
+provider "aws" {
+   region     = "us-east-1"
+   access_key = "AKIAYQLEG2NQUXHTBGIX"
+   secret_key = "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"
+}
+
+
+resource "aws_dynamodb_table" "state_locking" {
+  hash_key = "LockID"
+  name     = "dynamodb-state-locking"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  billing_mode = "PAY_PER_REQUEST"
+}
+
+resource "aws_instance" "ec2_example" {
+    ami = "ami-0c4f7023847b90238"
+    instance_type = "t2.micro"
+    tags = {
+      Name = "EC2 Instance with remote state"
+    }
+}
+
+terraform {
+    backend "s3" {
+        bucket = "vadims-terraform-s3-bucket"
+        key    = "vadims/terraform/remote/s3/terraform.tfstate"
+        region     = "us-east-1"
+    }
+}
