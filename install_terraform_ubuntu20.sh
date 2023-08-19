3e743652ef5e4973c61b4431e3fe833c182090fa
Vadim Sohin , DevOps  Engineer <vadim.sohin@gmail.com>
2023-08-19 16:02:47 +0200

    Create install_terraform_ubuntu20.sh

---
+++ install_terraform_ubuntu20.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+apt-get install wget curl unzip software-properties-common gnupg2 -y
+curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
+apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+apt-get update -y
+apt-get install terraform -y
+terraform -v
