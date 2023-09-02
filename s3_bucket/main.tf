fcdbd58c53000fa267235131648eee9894ad5210
Vadim Sohin , DevOps  Engineer <vadim.sohin@gmail.com>
2023-09-02 15:49:37 +0200

    Update main.tf

--- s3_bucket/main.tf
+++ s3_bucket/main.tf
@@ -1,1 +1,16 @@
+# this tf file will create s3 bucket with folder 
 
+provider "aws" {
+  region = "us-east-2" # Replace with your desired AWS region
+}
+resource "aws_s3_bucket" "labs-devops" {
+  bucket = "vadimso-devops-stuff" # Replace with your desired bucket name
+  #aws_s3_bucket_acl    = "public-read-write"            # Access control list for the bucket (can be "private", "public-read", "public-read-write", etc.)
+}
+
+resource "aws_s3_bucket_object" "python-scripts-folder" {
+  bucket = aws_s3_bucket.labs-devops.id
+  key    = "python-scripts/" # Note the trailing slash to create a "folder"
+  source = "/dev/null"  # You can use any content or file as the source
+  etag   = "d41d8cd98f00b204e9800998ecf8427e" # MD5 checksum for an empty file
+}
