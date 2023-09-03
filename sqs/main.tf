3642071033634327b1246ad2e0bf9e8e36106aa5
Vadim Sohin , DevOps  Engineer <vadim.sohin@gmail.com>
2023-09-03 21:04:44 +0200

    Update main.tf

--- sqs/main.tf
+++ sqs/main.tf
@@ -1,1 +1,54 @@
-# this config will create sqs queue and iam policy 
+# this config will create sqs queue and iam policy
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region                   = "us-east-1"
+#  shared_config_files      = ["/Path/to/.aws/config"]
+#  shared_credentials_files = ["/Path/to/.aws/credentials"]
+#  profile                  = "PROFILE"
+}
+
+resource "aws_sqs_queue" "sh_queue" {
+  name                       = "sh-example-queue"
+  delay_seconds              = 10
+  visibility_timeout_seconds = 30
+  max_message_size           = 2048
+  message_retention_seconds  = 86400
+  receive_wait_time_seconds  = 2
+  sqs_managed_sse_enabled = true
+}
+
+
+data "aws_iam_policy_document" "sh_sqs_policy" {
+  statement {
+    sid    = "shsqsstatement"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "sqs:SendMessage",
+      "sqs:ReceiveMessage"
+    ]
+    resources = [
+      aws_sqs_queue.sh_queue.arn
+    ]
+  }
+}
+
+resource "aws_sqs_queue_policy" "sh_sqs_policy" {
+  queue_url = aws_sqs_queue.sh_queue.id
+  policy    = data.aws_iam_policy_document.sh_sqs_policy.json
+}
